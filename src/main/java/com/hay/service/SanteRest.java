69c90ee4546825753a9dd2108137fd7f602bb82b
HAMZA AIT OUMGHAR <hamwac5@yahoo.fr>
2018-05-15 20:43:20 +0000

    Update Sante Contrat 15/05

--- src/main/java/com/hay/service/SanteRest.java
+++ src/main/java/com/hay/service/SanteRest.java
@@ -1,5 +1,7 @@
 package com.hay.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +37,11 @@ public class SanteRest {
 		service.setDossier(dossier);
 		
 		return  repoSante.save(service);
+	}
+	
+	@RequestMapping(value="/Sante",method=RequestMethod.GET)
+	public List<Sante> getAllContratSante(){
+			return this.repoSante.findAll();
 	}
 	
 	
